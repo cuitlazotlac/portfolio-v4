10344f117c6c6bef39cf602e4a82fee23780a555
cuitlazotlac <cuitlazotlac@gmail.com>
2022-08-09 00:42:00 -0400

    functions and styles implementation

---
+++ src/utils/utils.js
@@ -0,0 +1,24 @@
+export const DEFAULT_IMAGE_URL = "https://imgur.com/5dYYce8.png";
+export const AvatarImage = "https://imgur.com/yaefpD9.png";
+export const homeProfileImage = "https://imgur.com/mKrXwWF.png";
+export const resumeDownloadLink =
+  "https://drive.google.com/u/0/uc?id=1TgQMn35p_UZ-yOMGxUPfLK6IT4sM-5ZK&export=download";
+export const navigationRoutes = [
+  "home",
+  "stats",
+  "utilities",
+  "blogs",
+  "certificates",
+  "projects",
+  "newsletter",
+  "rss",
+];
+
+export const pagePreviewImage = {
+  home: "https://imgur.com/oYlyddc.png",
+  stats: "https://imgur.com/eSS84ff.png",
+  utilities: "https://imgur.com/jNsHxeu.png",
+  blogs: "https://imgur.com/Anrnix9.png",
+  certificates: "https://imgur.com/GyWZVXc.png",
+  projects: "https://imgur.com/sJmnqGt.png",
+};
